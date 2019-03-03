895f5cc815d004479ff87b6600f54db470d1b1ef
youzn99 <youzn@buaa.edu.cn>
2019-03-03 20:41:41 +0800

    version-2.0 abstract poly unit as a class, fix mistakes in regex

--- src/Poly.java
+++ src/Poly.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -8,22 +9,22 @@ public class Poly {
 
     // 3 states of poly units: 1) C*x^n. 2)C. 3)[+-]x^n
     private static String polyUnitRegex =
-            "(" +
-                    "([+-]?\\d+\\s*\\*\\s*x(\\s*\\^\\s*[+-]?\\d+)?)|" +
-                    "([+-]?\\d+)|" +
-                    "([+-]?x(\\s*\\^\\s*[+-]?\\d+)?)" +
-                    ")";
+        "(" +
+            "([+-]?\\d+\\s*\\*\\s*x(\\s*\\^\\s*[+-]?\\d+)?)|" +
+            "([+-]?\\d+)|" +
+            "([+-]?\\s*x(\\s*\\^\\s*[+-]?\\d+)?)" +
+            ")";
     private static String polyRegex =
-            "\\s*" +
-                    "\\s*[+-]?\\s*" + polyUnitRegex + "\\s*" +
-                    "(" + "\\s*[+-]\\s*" + polyUnitRegex + "\\s*" + ")*" +
-                    "\\s*";
+        "\\s*" +
+            "\\s*[+-]?\\s*" + polyUnitRegex + "\\s*" +
+            "(" + "\\s*[+-]\\s*" + polyUnitRegex + "\\s*" + ")*" +
+            "\\s*";
     // valid of input expression
     private boolean valid;
     // record appeared x's index
-    private ArrayList<Integer> indexList = new ArrayList<>();
+    private ArrayList<BigInteger> indexList = new ArrayList<>();
     // record coefficient of corresponding index
-    private HashMap<Integer, Integer> coeMap = new HashMap<>();
+    private HashMap<BigInteger, PolyUnit> coeMap = new HashMap<>();
 
     /**
      * read expression and construct object
@@ -36,13 +37,34 @@ public class Poly {
 
         // split valid expression into polyUnit
         if (valid) {
-            str = polySimplify(str);
+            String strNew = polySimplify(str);
             Pattern polyUnitPattern = Pattern.compile(polyUnitRegex);
-            Matcher polyUnitMatch = polyUnitPattern.matcher(str);
+            Matcher polyUnitMatch = polyUnitPattern.matcher(strNew);
             while (polyUnitMatch.find()) {
-                polyUnitRecord(polyUnitMatch.group(0));
+                PolyUnit polyUnit = new PolyUnit(polyUnitMatch.group(0));
+                // add index in indexList
+                BigInteger index = polyUnit.getIndex();
+                BigInteger coe = polyUnit.getCoe();
+                if (!indexList.contains(index)) {
+                    indexList.add(index);
+                }
+                // update(or add) Coefficient map
+                if (coeMap.containsKey(index)) {
+                    PolyUnit temp = coeMap.get(index);
+                    temp.updateCoe(coe);
+                    coeMap.replace(index, temp);
+                } else {
+                    coeMap.put(index, polyUnit);
+                }
             }
         }
+    }
+
+    /**
+     * check format validation of poly
+     */
+    public boolean formatCheck() {
+        return valid;
     }
 
     /**
@@ -54,118 +76,6 @@ public class Poly {
         Pattern polyPattern = Pattern.compile(polyRegex);
         Matcher polyMatch = polyPattern.matcher(str);
         return polyMatch.matches();
-    }
-
-    /**
-     * analysis unit information and update indexMap and coeList
-     *
-     * @param str string of poly unit
-     */
-    private void polyUnitRecord(String str) {
-//        System.out.println(str);
-        int coe, index;
-        // take out coefficient and index in total 5 situation
-        if (str.matches("[+-]?\\d+")) {
-            coe = Integer.parseInt(str);
-            index = 0;
-        } else if (str.matches("[+-]?x")) {
-            coe = 1;
-            index = 1;
-        } else if (str.matches("[+-]?x\\^[+-]?\\d+")) {
-            coe = 1;
-            index = Integer.parseInt(str.substring(str.indexOf("^") + 1));
-        } else if (str.matches("[+-]?\\d+\\*x")) {
-            coe = Integer.parseInt(str.substring(0, str.indexOf("*")));
-            index = 1;
-        } else {
-            coe = Integer.parseInt(str.substring(0, str.indexOf("*")));
-            index = Integer.parseInt(str.substring(str.indexOf("^") + 1));
-        }
-
-        // add index in indexList and update coe map
-        if (!indexList.contains(index)) {
-            indexList.add(index);
-        }
-        if (coeMap.containsKey(index)) {
-            int oriValue = coeMap.get(index);
-            coeMap.replace(index, oriValue + coe);
-        } else {
-            coeMap.put(index, coe);
-        }
-    }
-
-    /**
-     * delete empty space and replace double operation
-     *
-     * @param str original str
-     */
-    private String polySimplify(String str) {
-        // basic simplify
-        str = str.replaceAll("\\s", "");
-        str = str.replaceAll("(\\+-)|(-\\+)", "-");
-        str = str.replaceAll("(\\+{2})|(-{2})", "+");
-        // advance simplify
-        str = str.replaceAll("x\\^\\+?1","x");
-        str = str.replaceAll("\\*x\\^[+-]?0","");
-        str = str.replaceAll("1\\*x]","x");
-
-        return str;
-    }
-
-    /**
-     * given poly unit's coefficient and index return simplified unit's expression
-     *
-     * @param coe   coefficient
-     * @param index index
-     * @return simplified format of unit
-     */
-    private String unitGenerate(int coe, int index) {
-        StringBuffer strb = new StringBuffer();
-        // special occasion coefficient = 0
-        if(coe==0){
-            return strb.toString();
-        }
-        else{
-            strb.append("+");
-            // coe
-            strb.append(coe);
-            strb.append("*x");
-            // index
-            strb.append("^");
-            strb.append(index);
-            return strb.toString();
-        }
-    }
-
-    /**
-     * generate poly of original poly given derivation number
-     *
-     * @param derivationNum times of derivative
-     * @return poly expression
-     */
-    private String polyGenerate(int derivationNum) {
-        // sort by index
-        indexList.sort(Collections.reverseOrder());
-        // generate message
-        StringBuffer strBuf = new StringBuffer();
-        strBuf.append("0");
-        for (int i = 0; i < indexList.size(); i++) {
-            int index = indexList.get(i);
-            int coe = coeMap.get(index);
-            for (int j = 0; j < derivationNum; j++) {
-                coe *= index;
-                index -= 1;
-            }
-            strBuf.append(unitGenerate(coe, index));
-        }
-        return strBuf.toString();
-    }
-
-    /**
-     * check format validation of poly
-     */
-    public boolean formatCheck() {
-        return valid;
     }
 
     /**
@@ -186,32 +96,44 @@ public class Poly {
         return polySimplify(polyGenerate(1));
     }
 
-    // test function
-    public static void main(String[] arg) {
-        ArrayList<String> strItem = new ArrayList<>();
-//        strItem.add("+ -5*x^5 - 6 + 7*x + x^5");
-//        strItem.add("1926 0817 * x");
-        strItem.add("1");
-        strItem.add("4*x+x^2+x");
-        strItem.add("4*x+x^2+x");
-        strItem.add("4x+x^2+x");
-        strItem.add("- -4*x + x ^ 2 + x");
-        strItem.add("+4*x - -x^2 + x");
-        strItem.add("+19260817*x");
-        strItem.add("+ 19260817*x");
-        strItem.add("+ +19260817*x");
-        strItem.add("++ 19260817*x");
-        strItem.add("1926 0817 * x");
-        strItem.add(" ");
-        strItem.add("");
-        for(int i=0;i<strItem.size();i++){
-            Poly poly = new Poly(strItem.get(i));
-            if(poly.formatCheck()){
-                System.out.print(poly.getOriginalPoly()+"  <------->  ");
-                System.out.println(poly.getDerivativePoly());
-            }else {
-                System.out.println("WRONG FORMAT!");
-            }
-        }
+
+    /**
+     * delete empty space and replace double operation
+     *
+     * @param str original str
+     */
+    private String polySimplify(String str) {
+
+        String newStr = str;
+        // basic simplify
+        newStr = newStr.replaceAll("\\s", "");
+        newStr = newStr.replaceAll("(\\+-)|(-\\+)", "-");
+        newStr = newStr.replaceAll("(\\+{2})|(-{2})", "+");
+        // advance simplify
+        newStr = newStr.replaceAll("x\\^\\+?1", "x");
+        newStr = newStr.replaceAll("\\*x\\^[+-]?0", "");
+        newStr = newStr.replaceAll("1\\*x]", "x");
+        return newStr;
     }
+
+
+    /**
+     * generate poly of original poly given derivation number
+     *
+     * @param derivationNum times of derivative
+     * @return poly expression
+     */
+    private String polyGenerate(int derivationNum) {
+        // sort by index
+        indexList.sort(Collections.reverseOrder());
+        // generate message
+        StringBuffer strBuf = new StringBuffer();
+        strBuf.append("0");
+        for (int i = 0; i < indexList.size(); i++) {
+            PolyUnit temp = coeMap.get(indexList.get(i));
+            strBuf.append(temp.derivation(derivationNum));
+        }
+        return strBuf.toString();
+    }
+
 }
--- src/PolyUnit.java
+++ src/PolyUnit.java
@@ -1,13 +1,108 @@
 import java.math.BigInteger;
 
 public class PolyUnit {
-    public BigInteger coe;
-    public BigInteger index;
+    private BigInteger coe;
+    private BigInteger index;
 
-    PolyUnit(String str){
-
+    PolyUnit(String str) {
+        polyUnitRecord(str);
     }
-    PolyUnit(BigInteger coe, BigInteger index){
 
+    PolyUnit(BigInteger coe, BigInteger index) {
+        this.coe = coe;
+        this.index = index;
+    }
+
+    public String getUnit() {
+        return unitGenerate(this.coe, this.index);
+    }
+
+    public BigInteger getCoe() {
+        return coe;
+    }
+
+    public BigInteger getIndex() {
+        return index;
+    }
+
+    /**
+     * update coefficient of a unit.
+     *
+     * @param newCoe
+     */
+    public void updateCoe(BigInteger newCoe) {
+        this.coe = this.coe.add(newCoe);
+    }
+
+    public String derivation(int derivationNum) {
+        BigInteger index = this.index;
+        BigInteger coe = this.coe;
+        for (int j = 0; j < derivationNum; j++) {
+            coe = coe.multiply(index);
+            index = index.add(new BigInteger("-1"));
+        }
+        return unitGenerate(coe, index);
+    }
+
+    /**
+     * given poly unit's coefficient and index return simplified
+     * unit's expression
+     *
+     * @param coe   coefficient
+     * @param index index
+     * @return simplified format of unit
+     */
+    private String unitGenerate(BigInteger coe, BigInteger index) {
+        StringBuffer strb = new StringBuffer();
+        // special occasion coefficient = 0
+        if (coe.equals(new BigInteger("0"))) {
+            return strb.toString();
+        } else {
+            strb.append("+");
+            // coe
+            strb.append(coe);
+            strb.append("*x");
+            // index
+            strb.append("^");
+            strb.append(index);
+            return strb.toString();
+        }
+    }
+
+    /**
+     * analysis unit information and update indexMap and coeList
+     *
+     * @param str string of poly unit
+     */
+    private void polyUnitRecord(String str) {
+        BigInteger coe = BigInteger.ZERO;
+        BigInteger index = BigInteger.ZERO;
+        // take out coefficient and index in total 5 situation
+        if (str.matches("[+-]?\\d+")) {
+            coe = new BigInteger(str);
+            index = new BigInteger("0");
+        } else if (str.matches("[+-]?x")) {
+            if (str.charAt(0) == '-') {
+                coe = new BigInteger("-1");
+            } else {
+                coe = new BigInteger("1");
+            }
+            index = new BigInteger("1");
+        } else if (str.matches("[+-]?x\\^[+-]?\\d+")) {
+            if (str.charAt(0) == '-') {
+                coe = new BigInteger("-1");
+            } else {
+                coe = new BigInteger("1");
+            }
+            index = new BigInteger(str.substring(str.indexOf("^") + 1));
+        } else if (str.matches("[+-]?\\d+\\*x")) {
+            coe = new BigInteger(str.substring(0, str.indexOf("*")));
+            index = new BigInteger("1");
+        } else {
+            coe = new BigInteger(str.substring(0, str.indexOf("*")));
+            index = new BigInteger(str.substring(str.indexOf("^") + 1));
+        }
+        this.index = index;
+        this.coe = coe;
     }
 }
