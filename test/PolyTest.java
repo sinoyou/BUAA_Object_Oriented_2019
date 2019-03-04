76368700a5fbcfaf020a27557992e2e3c76d0ebb
youzn99 <youzn@buaa.edu.cn>
2019-03-04 23:13:41 +0800

    Our families did it.

--- test/PolyTest.java
+++ test/PolyTest.java
@@ -1,5 +1,10 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 public class PolyTest {
 
+    @org.junit.Test
+    public void formatCheck() {
+        Poly poly = new Poly("x");
+        assertTrue(poly.formatCheck());
+    }
 }
