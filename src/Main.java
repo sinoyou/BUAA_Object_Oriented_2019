e4c008f78b78a8638ac46a2cfe52094618ac4b37
youzn99 <youzn@buaa.edu.cn>
2019-03-19 00:45:10 +0800

    add try-catch of main

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        if (scan.hasNextLine()) {
-            String line = scan.nextLine();
-            Parse parse = new Parse(line);
-            if (parse.isValid()) {
-                System.out.println(parse.getRoot().getDerivate().toString());
+        try{
+            if (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                Parse parse = new Parse(line);
+                if (parse.isValid()) {
+                    System.out.println(parse.getRoot().getDerivate().toString());
+                } else {
+                    System.out.println("WRONG FORMAT!");
+                    System.err.println("Can't meet primary request.");
+                }
             } else {
                 System.out.println("WRONG FORMAT!");
-                System.err.println("Can't meet primary request.");
+                System.err.println("Empty Line.");
             }
-        } else {
+        }catch (Exception e){
             System.out.println("WRONG FORMAT!");
-            System.err.println("Empty Line.");
+            System.err.println("Unknown Exception:"+e.toString());
         }
     }
 }
